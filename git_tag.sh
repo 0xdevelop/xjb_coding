ff87170bff6c5ebeeff45898cbe93cba13d42ab0
0xYeah <wmyeah017@gmail.com>
2026-09-03 14:09:10 +0800

    fix(release): handle Codex cachebuster versions

--- git_tag.sh
+++ git_tag.sh
@@ -126,7 +126,7 @@ function git_handle_ready() {
         sed -i -e "s/\"version\": \"[0-9.]*\"/\"version\": \"${plain_version}\"/" "$PluginJsonFile"
     fi
     if [ -f "$CodexPluginJsonFile" ]; then
-        sed -i -E -e "s/(\"version\": \")[^\"]+(\")/\1${plain_version}\2/" "$CodexPluginJsonFile"
+        sed -i -E -e "s/\"version\": \"[^\"]+\"/\"version\": \"${plain_version}\"/" "$CodexPluginJsonFile"
     fi
 
     if [[ $OS_TYPE == "Darwin" ]]; then
